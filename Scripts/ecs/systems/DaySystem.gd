c1c44307993a8a0171a5d0904539f6243e2333d3
adoubt <gerya1508@gmail.com>
2026-02-05 04:20:05 +0700

    0.091 DaySystem _day_reward

--- Scripts/ecs/systems/DaySystem.gd
+++ Scripts/ecs/systems/DaySystem.gd
@@ -24,13 +24,14 @@ func _change_day(data: Dictionary={})->void:
 	if logs_before < 3:
 		_kill_players()
 		#SceneManager.go_to_intro()
-	
+	_day_reward()
 	event_bus.emit("day_changed", {"current_day": run_comp.current_day})
 	event_bus.emit("balance_changed", {"current_balance": run_comp.logs, "value": 3.0})
-func _on_combat_completed(data: Dictionary):
-	_reward(data)
 	
-func _reward(data: Dictionary):
+func _on_combat_completed(data: Dictionary):
+	_combat_reward(data)
+	
+func _combat_reward(data: Dictionary):
 	var day_e = data.current_day
 	var phase = data.current_phase
 	var day_index = data.day_index
@@ -40,7 +41,10 @@ func _reward(data: Dictionary):
 	cs.add_component(RUN, "BalanceChangeRequestComponent", BalanceChangeRequestComponent.new(log_reward, "combat_reward",day_e))
 
 func _kill_players()->void:
-	var entities = player_arch.entities.duplicate()
-	for e in entities:
-		cs.add_component(e,"DeathRequestComponent", DeathRequestComponent.new(RUN))
 	
+	for e in player_arch.entities:
+		cs.add_component(e,"DeathRequestComponent", DeathRequestComponent.new(RUN))
+func _day_reward()->void:
+	for e in player_arch.entities:
+		cs.get_component(e, "SlotsCountComponent").base_value+=1
+		cs.add_component(e, "DirtyStatsComponent",DirtyStatsComponent.new())
